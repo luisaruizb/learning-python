e7c2d90c4b6b574efb6d9833eaaade6225c12666
Luisa <luisaruizbucheli@gmail.com>
2024-01-20 09:09:03 -0500

    Add new feature: age calc

--- welcome.py
+++ welcome.py
@@ -1,6 +1,9 @@
 name = "luisa"
 country = "colombia"
+birthday = 1998
+now = 2024
 
 print ("you are ", name, "and you from", country)
 print("you are",name)
 print ("you are from", country)
+print("your age is", now-birthday)
