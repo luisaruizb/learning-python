c99e6f05343b56a4917277aea4732e33ed87d16e
Luisa <luisaruizbucheli@gmail.com>
2024-01-20 09:34:29 -0500

    his calculator let you get the result of 4 basic  math operations v1 and v2

---
+++ basic_calculator.py
@@ -0,0 +1,29 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+... '''DESCRIPTION: 
+... this calculator let you get the result of 4 basic  math operations
+... from two numbers (variables)
+... '''
+... #IMPUTS (two static variables)
+... num1 = 20
+... num2 = 3
+... 
+... #PROCESS
+... add= num1+num2
+... sub= num1 - num2
+... mul= num1 * num2
+... div = num1 / num2
+... 
+... #OUTPUTS 
+... print(":::BASIC CALCULATOR v1::::")
+... 
+... print("number 1 :", num1, type(num1))
+... print("number 2 :", num2, type(num2))
+... 
+... print("the addition is:", add,type(add))
+... print("the subtractio is:", sub,type(sub))
+... print("the multipication is:", mul,type(mul))
+... print("the dicision is:", div,type(div))
+... 
+... print("\n")
