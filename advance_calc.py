dfc65e092d036cf16a251b0e2bff6322e70361f2
Luisa <luisaruizbucheli@gmail.com>
2024-01-20 13:08:24 -0500

    upload advance calc and aleatorios1

---
+++ advance_calc.py
@@ -0,0 +1,13 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+import os
+
+os.system('clear')
+
+#inputs
+print("ingrese primer valor")
+n1 = int(input())
+n1 = int(input("ingrese el segundo valor"))
+suma = n1 + n2
+print ("suma" , suma)
