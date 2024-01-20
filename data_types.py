70475d20d23af6ac141603152c0b73ee39233977
Luisa <luisaruizbucheli@gmail.com>
2024-01-20 12:10:04 -0500

    explicacion de tipos de datso en python

---
+++ data_types.py
@@ -0,0 +1,44 @@
+# tipos de datos en pytho 
+#pyton es un lenguaje dimanicamnete tipado 
+# cunado se declara una variable no es nesesario 
+# especificarlo 
+
+#1.numericos
+
+num1 = 42
+print ( "num1:" , type(num1))
+
+#2 cadena
+
+my_name = "luisa"
+print ("my_name", type (my_name))
+my_lastname ='''ruiz kkklll'''
+print ("my_lastname", type (my_lastname))
+
+a = 1
+b = 1
+
+print (a + b)
+
+c = 1
+d = '1'
+
+#3 logicos
+
+usuario_activo = True
+print ("Usuario_activo", type (usuario_activo))
+usuario_inactivo = False
+print ("Usuario_inactivo", type (usuario_inactivo))
+
+#4 listas
+
+frutas = ['apple' , 'banana']
+colors = ['rojo' , 'negro' , 'azul']
+print (frutas)
+print ("colors", type (colors))
+
+
+#5 tuplas
+#6 diccionarios
+#7 conjuntos
+
