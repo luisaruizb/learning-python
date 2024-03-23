7be18bee7b890cec6204073cf30bc80fb181263d
Luisa <luisaruizbucheli@gmail.com>
2024-03-23 11:38:36 -0500

    Actualizar archivos modificados y eliminar archivo crud_school.py

--- Data_mine/School/school_db.py
+++ Data_mine/School/school_db.py
@@ -1,60 +1,124 @@
-from school_db import con, cur
-import os
-import bcrypt
+#Import engine database package
+import sqlite3
 
-status_menu = True
-global status_op
+#Create a database connection (database name)
+con = sqlite3.connect('school.db')
 
-def hash_password(passwd):
-    return bcrypt.hashpw(passwd.encode(), bcrypt.gensalt())
+#creating cursor object 
+cur = con.cursor()
 
-def create_user(op):
-    #Create a new user
-    os.system('clear')
+# Creación de las tablas
 
-    print("::: Signup form :::")
-    idusers = input("Your id_users: ")
-    email = input("Your email: ")
-    passwd = input("Your password: ")
-    passwd_hashed = hash_password(passwd)
-    status = input("Your status: ")
-    created= input("Your created_at: ")
-    updated= input("Your updated_at ")
-    deleted= input("Your deleted_at: ")
-  
-    new_user = f'''
-        INSERT INTO 
-            Users ( id_users,  email, password, status, created_at,updated_at,deleted_at) 
-            VALUES('{idusers}', '{email}', "{passwd_hashed}", '{status}', '{created}', '{updated}', '{deleted}')
-    '''
-    con.execute(new_user)
-    con.commit()
+Students_table='''
+    CREATE TABLE IF NOT EXISTS Students (
+        id_Student INTEGER PRIMARY KEY NOT NULL,
+        code VARCHAR(50) NOT NULL,
+        id_persons INTEGER,
+        status BOOLEAN,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        deleted_at DATETIME,
+        FOREIGN KEY (id_persons) REFERENCES Persons(id_persons)
+    );
+'''
 
-    print("::: New user has been created sucessfully :::")
-    os.system('pause')
-    menu()
+Persons_table='''
+    CREATE TABLE IF NOT EXISTS Persons (
+        id_persons INTEGER PRIMARY KEY, 
+        first_name VARCHAR(50) NOT NULL, 
+        last_name VARCHAR(50),
+        id_ident_type INTEGER,
+        ident_number VARCHAR(15) NOT NULL,
+        id_exp_City INTEGER,
+        address VARCHAR(150) NOT NULL,
+        mobile VARCHAR(50) NOT NULL,
+        id_users INTEGER,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        deleted_at DATETIME,
+        FOREIGN KEY (id_ident_type) REFERENCES Identification_types(id_ident_type),
+        FOREIGN KEY (id_exp_City) REFERENCES City(id_exp_City),
+        FOREIGN KEY (id_users) REFERENCES Users(id_users)
+    );
+'''
 
-def create_student(op):
-    #Create a new user
-    os.system('clear')
+Users_table='''
+    CREATE TABLE IF NOT EXISTS Users (
+        id_users INTEGER PRIMARY KEY NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        password VARCHAR(250) NOT NULL,
+        status BOOLEAN,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        deleted_at DATETIME
+    );
+'''
 
-    print("::: Signup form :::")
-    idstuds = input("Your id_Students: ")
-    code= input("your code : ")
-    id_persons=input("your id_persons : ")
-    status = input("Your status: ")
-    created= input("Your created_at: ")
-    updated= input("Your updated_at ")
-    deleted= input("Your deleted_at: ")
-  
-    new_student = f'''
-        INSERT INTO 
-            Students ( id_Student, code, status, created_at,updated_at,deleted_at) 
-            VALUES('{idstuds}', '{code}','{status}', '{created}', '{updated}', '{deleted}')
-    '''
-    con.execute(new_student)
-    con.commit()
+Identification_types_table='''
+    CREATE TABLE IF NOT EXISTS Identification_types (
+        id_ident_type INTEGER PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        abrev VARCHAR(10) NOT NULL,
+        descrip VARCHAR(100) NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        deleted_at DATETIME
+    );
+'''
 
-    print("::: New user has been created sucessfully :::")
-    os.system('pause')
-    menu()
+City_table='''
+    CREATE TABLE IF NOT EXISTS City (
+        id_exp_City INTEGER PRIMARY KEY,
+        name VARCHAR(50) NOT NULL,
+        abrev VARCHAR(10) NOT NULL,
+        descrip VARCHAR(100) NOT NULL,
+        id_depart INTEGER,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        deleted_at DATETIME,
+        FOREIGN KEY (id_depart) REFERENCES Department(id_depart)
+    );
+'''
+
+Departaments_table='''
+    CREATE TABLE IF NOT EXISTS Department (
+        id_depart INTEGER PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        abrev VARCHAR(10) NOT NULL,
+        descrip VARCHAR(10) NOT NULL,
+        id_country INTEGER,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        deleted_at DATETIME,
+        FOREIGN KEY (id_country) REFERENCES Country(id_country)
+    );
+'''
+
+
+Country_table='''
+    CREATE TABLE IF NOT EXISTS Country (
+        id_country INTEGER PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        abrev VARCHAR(10) NOT NULL,
+        descrip VARCHAR(10) NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME NOT NULL,
+        deleted_at DATETIME,
+        FOREIGN KEY (id_country) REFERENCES Department(id_country)
+    );
+'''
+
+
+
+cur.execute(Students_table)
+cur.execute(Persons_table)
+cur.execute(Users_table)
+cur.execute(Identification_types_table)
+cur.execute(City_table)
+cur.execute(Departaments_table)
+cur.execute(Country_table)
+#Save changes in database
+con.commit()
+
+
+#print("Base de datos 'school' creada con éxito.")
